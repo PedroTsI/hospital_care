908a5c302c48328a6e019c29cea93ca0a02f7b56
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-09-21 00:05:37 -0300

    First Study React Basic

---
+++ react_study/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { GeoForm } from "@/components/GeoForm";
+import { Person } from "@/components/Person";
+
+const Page = () => {
+  return (
+  <div>
+    <Person/>
+    <GeoForm/>
+  </div>
+  );
+}
+
+export default Page;
